2f51b3befc87b7239cea33f58ac2d76ed8f47ba8
warach7 <69473931+warach7@users.noreply.github.com>
2020-09-22 17:56:01 +0700

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -13,21 +13,23 @@ MR.WARACH CHANGWATTHANAKUL
 """)
 
 START = "timestart"
-day = st.slider("Day to look at", 1, 4, step = 1)
+day = st.slider("Day to look at", 1, 5, step = 1)
 if day == 1:
     DATA_URL = ("https://raw.githubusercontent.com/warach7/STREAMLIT_HW/master/20190101.csv")
-elif day == 2:
+if day == 2:
+    DATA_URL = ("https://raw.githubusercontent.com/warach7/STREAMLIT_HW/master/20190102.csv")
+if day == 3:
     DATA_URL = ("https://raw.githubusercontent.com/warach7/STREAMLIT_HW/master/20190103.csv")
-elif day == 3:
+if day == 4:
     DATA_URL = ("https://raw.githubusercontent.com/warach7/STREAMLIT_HW/master/20190104.csv")
-elif day == 4:
+if day == 5:
     DATA_URL = ("https://raw.githubusercontent.com/warach7/STREAMLIT_HW/master/20190105.csv")
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis="columns", inplace=True)
-    data[START] = pd.to_datetime(data[START], format = '%d/%m/%Y %H:%M')
+    data[START] = pd.to_datetime(data[START], format = '%d-%m-%y %H:%M')
     return data
 
 data = load_data(100000)
@@ -35,19 +37,16 @@ data = load_data(100000)
 hour = st.slider("Hour to look at", 0, 23, step = 3)
 
 data = data[data[START].dt.hour == hour]
-data = data.drop(data.columns[0], axis=1)
-for i in range(5):
-    data = data.drop(data.columns[6], axis=1)
 
 st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 3) % 24))
-midpoint = (np.average(data["latstartl"]), np.average(data["lonstartl"]))
+midpoint = (13.786331, 100.551762)
 
 st.write(pdk.Deck(
     map_style="mapbox://styles/mapbox/dark-v9",
     initial_view_state={
         "latitude": midpoint[0],
         "longitude": midpoint[1],
-        "zoom": 11,
+        "zoom": 10.5,
         "pitch": 50,
     },
     layers=[
