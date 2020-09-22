0b41d6eb35df2829240f9c4d0130714aef280fd2
warach7 <69473931+warach7@users.noreply.github.com>
2020-09-22 18:08:05 +0700

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -13,6 +13,7 @@ MR.WARACH CHANGWATTHANAKUL
 """)
 
 START = "timestart"
+STOP = "timestop"
 day = st.slider("Day to look at", 1, 5, step = 1)
 if day == 1:
     DATA_URL = ("https://raw.githubusercontent.com/warach7/STREAMLIT_HW/master/20190101.csv")
@@ -30,6 +31,7 @@ def load_data(nrows):
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis="columns", inplace=True)
     data[START] = pd.to_datetime(data[START], format = '%d-%m-%y %H:%M')
+    data[STOP] = pd.to_datetime(data[STOP], format = '%d-%m-%y %H:%M')
     return data
 
 data = load_data(100000)
@@ -37,12 +39,13 @@ data = load_data(100000)
 hour = st.slider("Hour to look at", 0, 23, step = 3)
 
 data = data[data[START].dt.hour == hour]
+data = data.drop(data.columns[0], axis=1)
 
-st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 3) % 24))
+st.subheader("Geo data START between %i:00 and %i:00" % (hour, (hour + 3) % 24))
 midpoint = (13.786331, 100.551762)
 
 st.write(pdk.Deck(
-    map_style="mapbox://styles/mapbox/dark-v9",
+    map_style="mapbox://styles/mapbox/dark",
     initial_view_state={
         "latitude": midpoint[0],
         "longitude": midpoint[1],
@@ -63,11 +66,52 @@ st.write(pdk.Deck(
     ],
 ))
 
-st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
+st.subheader("Geo data STOP between %i:00 and %i:00" % (hour, (hour + 3) % 24))
+midpoint = (13.786331, 100.551762)
+
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/dark-v9",
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 10.5,
+        "pitch": 50,
+    },
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=data,
+            get_position=["lonstop", "latstop"],
+            radius=150,
+            elevation_scale=5,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+    ],
+))
+
+st.subheader("START by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
 filtered = data[
     (data[START].dt.hour >= hour) & (data[START].dt.hour < (hour + 3))
 ]
 hist = np.histogram(filtered[START].dt.minute, bins=60, range=(0, 60))[0]
+chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
+
+st.altair_chart(alt.Chart(chart_data)
+    .mark_area(
+        interpolate='step-after',
+    ).encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=['minute', 'pickups']
+    ), use_container_width=True)
+
+st.subheader("STOP by minute between %i:00 and %i:00" % (hour, (hour + 3) % 24))
+filtered = data[
+    (data[STOP].dt.hour >= hour) & (data[STOP].dt.hour < (hour + 3))
+]
+hist = np.histogram(filtered[STOP].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
 
 st.altair_chart(alt.Chart(chart_data)
